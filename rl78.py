0f05b2975b429fad4b7332fa973080c904052bff
John McMaster <johndmcmaster@gmail.com>
2022-03-31 13:11:59 -0700

    use "self" instead of "s"
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- rl78.py
+++ rl78.py
@@ -24,24 +24,24 @@ WIRE_BLUE = 1 << 7
 
 
 class Reset:
-    def __init__(s, url):
+    def __init__(self, url):
         # init gpio mode with gray (conncted to RESET) and green (TOOL0) as outputs
-        s.gpio = GpioController()
-        s.gpio.open_from_url(url, direction=WIRE_GRAY | WIRE_GREEN)
+        self.gpio = GpioController()
+        self.gpio.open_from_url(url, direction=WIRE_GRAY | WIRE_GREEN)
 
-    def enter_rom(s):
-        s.gpio.set_direction(WIRE_GRAY | WIRE_GREEN)
+    def enter_rom(self):
+        self.gpio.set_direction(WIRE_GRAY | WIRE_GREEN)
         # RESET=0, TOOL0=0
-        s.gpio.write_port(0)
+        self.gpio.write_port(0)
         delay(.04)
         # RESET=1, TOOL0=0
-        s.gpio.write_port(WIRE_GRAY)
+        self.gpio.write_port(WIRE_GRAY)
         delay(.001)
         # RESET=1, TOOL0=1
-        s.gpio.write_port(WIRE_GRAY | WIRE_GREEN)
+        self.gpio.write_port(WIRE_GRAY | WIRE_GREEN)
         delay(.01)
         # stop driving TOOL0 (with this ftdi device - another one takes over)
-        s.gpio.set_direction(WIRE_GRAY)
+        self.gpio.set_direction(WIRE_GRAY)
 
 
 def read_all(port, size):
@@ -93,119 +93,120 @@ class ProtoA:
     ST_BLANK_ERR = 0x1b
     ST_WRITE_ERR = 0x1c
 
-    def __init__(s, port):
-        s.port = port
+    def __init__(self, port):
+        self.port = port
 
-    def read_all(s, size):
-        return read_all(s.port, size)
+    def read_all(self, size):
+        return read_all(self.port, size)
 
-    def _checksum(s, data):
+    def _checksum(self, data):
         csum = 0
         for d in data:
             csum -= d
             csum &= 0xff
         return csum
 
-    def _checksum16(s, data):
+    def _checksum16(self, data):
         csum = 0
         for d in data:
             csum -= d
             csum &= 0xffff
         return csum
 
-    def recv_frame(s):
-        while s.port.read() != bytes([s.STX]):
+    def recv_frame(self):
+        while self.port.read() != bytes([self.STX]):
             pass
-        len_b = s.port.read()
+        len_b = self.port.read()
         LEN = size8(struct.unpack('B', len_b)[0])
         recv_len = LEN + 2
-        data = s.read_all(recv_len)
-        #print('recv %s' % (binascii.hexlify(data)))
-        if s._checksum(len_b + data[:LEN]) != data[LEN]:
+        data = self.read_all(recv_len)
+        #print('recv %self' % (binascii.hexlify(data)))
+        if self._checksum(len_b + data[:LEN]) != data[LEN]:
             print('bad checksum')
-        if data[LEN + 1] != s.ETX:
+        if data[LEN + 1] != self.ETX:
             print('bad footer')
         return data[:LEN]
 
-    def _send_frame(s, data, is_cmd=True, last_data=True):
-        header = s.SOH if is_cmd else s.STX
-        trailer = s.ETX if last_data else s.ETB
+    def _send_frame(self, data, is_cmd=True, last_data=True):
+        header = self.SOH if is_cmd else self.STX
+        trailer = self.ETX if last_data else self.ETB
         LEN = size8(len(data))
-        SUM = s._checksum(struct.pack('B', LEN) + data)
+        SUM = self._checksum(struct.pack('B', LEN) + data)
         cmd = struct.pack('BB%dBBB' % (len(data)), header, LEN, *data, SUM,
                           trailer)
-        #print('send %s' % (binascii.hexlify(cmd)))
-        s.port.write(cmd)
+        #print('send %self' % (binascii.hexlify(cmd)))
+        self.port.write(cmd)
         # discard the loopback bytes
-        s.read_all(len(cmd))
-        return s.recv_frame()
+        self.read_all(len(cmd))
+        return self.recv_frame()
 
-    def send_frame(s, data, is_cmd=True, last_data=True):
+    def send_frame(self, data, is_cmd=True, last_data=True):
         while True:
-            r = s._send_frame(data, is_cmd, last_data)
-            if r[0] != s.ST_SUM_ERR:
+            r = self._send_frame(data, is_cmd, last_data)
+            if r[0] != self.ST_SUM_ERR:
                 return r
 
-    def reset(s):
-        return s.send_frame(struct.pack('B', s.COM_RESET))
+    def reset(self):
+        return self.send_frame(struct.pack('B', self.COM_RESET))
 
-    def set_baudrate(s, baudrate, voltage):
-        return s.send_frame(
-            struct.pack('BBB', s.COM_BAUDRATE_SET, baudrate, voltage))
+    def set_baudrate(self, baudrate, voltage):
+        return self.send_frame(
+            struct.pack('BBB', self.COM_BAUDRATE_SET, baudrate, voltage))
 
-    def silicon_sig(s):
-        r = s.send_frame(struct.pack('B', s.COM_SILICON_SIG))
-        if r[0] != s.ST_ACK: return None
-        return s.recv_frame()
+    def silicon_sig(self):
+        r = self.send_frame(struct.pack('B', self.COM_SILICON_SIG))
+        if r[0] != self.ST_ACK: return None
+        return self.recv_frame()
 
-    def security_get(s):
-        r = s.send_frame(struct.pack('B', s.COM_SEC_GET))
-        if r[0] != s.ST_ACK: return None
-        return s.recv_frame()
+    def security_get(self):
+        r = self.send_frame(struct.pack('B', self.COM_SEC_GET))
+        if r[0] != self.ST_ACK: return None
+        return self.recv_frame()
 
-    def security_set(s, sec):
-        r = s.send_frame(struct.pack('B', s.COM_SEC_SET))
-        if r[0] != s.ST_ACK: return None
-        return s.send_frame(sec, False)[0] == s.ST_ACK
+    def security_set(self, sec):
+        r = self.send_frame(struct.pack('B', self.COM_SEC_SET))
+        if r[0] != self.ST_ACK: return None
+        return self.send_frame(sec, False)[0] == self.ST_ACK
 
-    def verify(s, addr, data):
+    def verify(self, addr, data):
         assert len(data) > 0
         SA = pack24(addr)
         EA = pack24(addr + len(data) - 1)
-        r = s.send_frame(struct.pack('B', s.COM_VERIFY) + SA + EA)
-        if r[0] != s.ST_ACK: return False
+        r = self.send_frame(struct.pack('B', self.COM_VERIFY) + SA + EA)
+        if r[0] != self.ST_ACK: return False
         for i in range(0, len(data), 0x100):
             last_data = len(data) - i <= 0x100
-            r = s.send_frame(data[i:i + 0x100], False, last_data)
-        return r[0] == s.ST_ACK and r[1] == s.ST_ACK
+            r = self.send_frame(data[i:i + 0x100], False, last_data)
+        return r[0] == self.ST_ACK and r[1] == self.ST_ACK
 
-    def checksum(s, addr, size):
+    def checksum(self, addr, size):
         assert size > 0
         SA = pack24(addr)
         EA = pack24(addr + size - 1)
-        r = s.send_frame(struct.pack('B', s.COM_CHECKSUM) + SA + EA)
-        if r[0] != s.ST_ACK: return None
-        return struct.unpack('<H', s.recv_frame())[0]
+        r = self.send_frame(struct.pack('B', self.COM_CHECKSUM) + SA + EA)
+        if r[0] != self.ST_ACK: return None
+        return struct.unpack('<H', self.recv_frame())[0]
 
-    def blank_check(s, addr, size=0x400):
+    def blank_check(self, addr, size=0x400):
         assert size > 0
         SA = pack24(addr)
         EA = pack24(addr + size - 1)
         # XXX
         D01 = struct.pack('B', 0)
-        r = s.send_frame(struct.pack('B', s.COM_BLANK_CHECK) + SA + EA + D01)
-        if r[0] not in (s.ST_ACK, s.ST_BLANK_ERR):
+        r = self.send_frame(
+            struct.pack('B', self.COM_BLANK_CHECK) + SA + EA + D01)
+        if r[0] not in (self.ST_ACK, self.ST_BLANK_ERR):
             return None
         # True means it is blank
-        return r[0] == s.ST_ACK
+        return r[0] == self.ST_ACK
 
-    def invert_boot_cluster(s):
+    def invert_boot_cluster(self):
         # XXX can't be set via protoA :'(
-        sec = s.security_get()
+        sec = self.security_get()
         sec = bytes([sec[0] ^ 1, *sec[1:]])
-        return s.security_set(sec)
+        return self.security_set(sec)
 
-    def cmd19(s):
+    def cmd19(self):
         # this is standalone "internal verify"
         addr = 0
         size = 0x400
@@ -213,35 +214,35 @@ class ProtoA:
         assert ((((addr + size - 1) >> 8) & 0xff) & 3) == 3
         SA = pack24(addr)
         EA = pack24(addr + size - 1)
-        return s.send_frame(struct.pack('B', s.COM_19) + SA + EA)
+        return self.send_frame(struct.pack('B', self.COM_19) + SA + EA)
 
-    def erase_block(s, addr):
-        return s.send_frame(struct.pack('B', s.COM_ERASE) + pack24(addr))
+    def erase_block(self, addr):
+        return self.send_frame(struct.pack('B', self.COM_ERASE) + pack24(addr))
 
-    def program(s, addr, data):
+    def program(self, addr, data):
         SA = pack24(addr)
         EA = pack24(addr + len(data) - 1)
-        r = s.send_frame(struct.pack('B', s.COM_PROG) + SA + EA)
-        if r[0] != s.ST_ACK: return False
+        r = self.send_frame(struct.pack('B', self.COM_PROG) + SA + EA)
+        if r[0] != self.ST_ACK: return False
         for i in range(0, len(data), 0x100):
             last_data = len(data) - i <= 0x100
-            r = s.send_frame(data[i:i + 0x100], False, last_data)
-        if r[0] != s.ST_ACK or r[1] != s.ST_ACK:
+            r = self.send_frame(data[i:i + 0x100], False, last_data)
+        if r[0] != self.ST_ACK or r[1] != self.ST_ACK:
             return False
         # iverify status
-        return s.recv_frame()
+        return self.recv_frame()
 
-    def write(s, addr, data):
+    def write(self, addr, data):
         # erase block = 0x400, everything else can use 0x100
         if addr % 0x400 or len(data) % 0x400:
             return False
         for i in range(0, len(data), 0x400):
-            s.erase_block(addr + i)
+            self.erase_block(addr + i)
         # XXX should be able to handle multiple blocks, not sure why it hangs
-        #s.program(addr, data)
+        #self.program(addr, data)
         for i in range(0, len(data), 0x100):
-            s.program(addr + i, data[i:i + 0x100])
-        return s.verify(addr, data)
+            self.program(addr + i, data[i:i + 0x100])
+        return self.verify(addr, data)
 
 
 class ProtoOCD:
@@ -262,13 +263,13 @@ class ProtoOCD:
     ST_UNLOCK_SUM = 0xf3
     ST_UNLOCK_NG = 0xf4
 
-    def __init__(s, port):
-        s.port = port
+    def __init__(self, port):
+        self.port = port
 
-    def read_all(s, size):
-        return read_all(s.port, size)
+    def read_all(self, size):
+        return read_all(self.port, size)
 
-    def checksum(s, data):
+    def checksum(self, data):
         csum = 0
         for d in data:
             csum += d
@@ -277,69 +278,69 @@ class ProtoOCD:
         csum &= 0xff
         return csum
 
-    def send_cmd(s, cmd):
-        #print('send %s' % (binascii.hexlify(cmd)))
-        s.port.write(cmd)
+    def send_cmd(self, cmd):
+        #print('send %self' % (binascii.hexlify(cmd)))
+        self.port.write(cmd)
         # discard the loopback bytes
-        s.read_all(len(cmd))
+        self.read_all(len(cmd))
 
-    def wait_ack(s):
-        while s.read_all(1) != bytes([s.SYNC]):
+    def wait_ack(self):
+        while self.read_all(1) != bytes([self.SYNC]):
             pass
 
-    def sync(s):
-        s.send_cmd(struct.pack('B', s.SYNC))
-        s.wait_ack()
+    def sync(self):
+        self.send_cmd(struct.pack('B', self.SYNC))
+        self.wait_ack()
 
-    def ping(s):
-        s.send_cmd(struct.pack('B', s.PING))
-        return s.read_all(len(s.PONG)) == s.PONG
-        #return s.read_all(len(ping_result)) == ping_result
-    def unlock(s, ocd_id, corrupt_sum=False):
-        s.send_cmd(struct.pack('B', s.UNLOCK))
-        status = s.read_all(1)[0]
+    def ping(self):
+        self.send_cmd(struct.pack('B', self.PING))
+        return self.read_all(len(self.PONG)) == self.PONG
+        #return self.read_all(len(ping_result)) == ping_result
+    def unlock(self, ocd_id, corrupt_sum=False):
+        self.send_cmd(struct.pack('B', self.UNLOCK))
+        status = self.read_all(1)[0]
         # f0: already unlocked
         # f1: need to send
-        if status == s.ST_UNLOCK_ALREADY:
+        if status == self.ST_UNLOCK_ALREADY:
             print('already unlocked')
             return True
-        if status != s.ST_UNLOCK_LOCKED:
+        if status != self.ST_UNLOCK_LOCKED:
             print('unexpected status')
             return False
-        csum = s.checksum(ocd_id)
+        csum = self.checksum(ocd_id)
         if corrupt_sum:
             csum += 1
             csum &= 0xff
-        s.send_cmd(struct.pack('10BB', *ocd_id, csum))
-        status = s.read_all(1)[0]
+        self.send_cmd(struct.pack('10BB', *ocd_id, csum))
+        status = self.read_all(1)[0]
         # f2: success
         # f3: checksum mismatch
         # f4: checksum matched but ocd_id did not (could trigger flash erase?)
-        if status != s.ST_UNLOCK_OK:
+        if status != self.ST_UNLOCK_OK:
             print('unlock failed: %x' % (status))
-        return status == s.ST_UNLOCK_OK
+        return status == self.ST_UNLOCK_OK
 
-    def read(s, offset, size):
+    def read(self, offset, size):
         size8_ = size8(size)
         if size8_ is None: return None
-        s.send_cmd(struct.pack('<BHB', s.READ, offset, size8_))
-        return s.read_all(size)
+        self.send_cmd(struct.pack('<BHB', self.READ, offset, size8_))
+        return self.read_all(size)
 
-    def write(s, addr, data):
+    def write(self, addr, data):
         size = size8(len(data))
         if size is None: return None
-        s.send_cmd(
-            struct.pack('<BHB%dB' (len(data)), s.WRITE, addr, size, *data))
-        return s.read_all(1)[0] == s.WRITE
+        self.send_cmd(
+            struct.pack('<BHB%dB' (len(data)), self.WRITE, addr, size, *data))
+        return self.read_all(1)[0] == self.WRITE
 
-    def call_f07e0(s):
-        s.send_cmd(struct.pack('B', s.EXEC))
-        return s.read_all(1)[0] == s.EXEC
+    def call_f07e0(self):
+        self.send_cmd(struct.pack('B', self.EXEC))
+        return self.read_all(1)[0] == self.EXEC
 
-    def leave(s, to_ram=False):
-        cmd = s.EXIT_RAM if to_ram else s.EXIT_RETI
-        s.send_cmd(struct.pack('B', cmd))
-        return s.read_all(1)[0] == cmd
+    def leave(self, to_ram=False):
+        cmd = self.EXIT_RAM if to_ram else self.EXIT_RETI
+        self.send_cmd(struct.pack('B', cmd))
+        return self.read_all(1)[0] == cmd
 
 
 class RL78:
@@ -349,40 +350,40 @@ class RL78:
     BAUDRATE_INIT = 115200
     BAUDRATE_FAST = 1000000
 
-    def __init__(s, gpio_url, uart_port):
-        s.reset_ctl = Reset(gpio_url)
-        s.port = serial.Serial(uart_port,
-                               baudrate=s.BAUDRATE_INIT,
-                               timeout=0,
-                               stopbits=2)
-        s.a = ProtoA(s.port)
-        s.ocd = ProtoOCD(s.port)
-        s.mode = None
+    def __init__(self, gpio_url, uart_port):
+        self.reset_ctl = Reset(gpio_url)
+        self.port = serial.Serial(uart_port,
+                                  baudrate=self.BAUDRATE_INIT,
+                                  timeout=0,
+                                  stopbits=2)
+        self.a = ProtoA(self.port)
+        self.ocd = ProtoOCD(self.port)
+        self.mode = None
 
-    def reset(s, mode):
-        s.mode = mode
-        s.port.baudrate = s.BAUDRATE_INIT
-        s.reset_ctl.enter_rom()
-        s.port.write(s.mode)
+    def reset(self, mode):
+        self.mode = mode
+        self.port.baudrate = self.BAUDRATE_INIT
+        self.reset_ctl.enter_rom()
+        self.port.write(self.mode)
         # we'll see the reset as a null byte. discard it and the init byte
-        read_all(s.port, 2)
+        read_all(self.port, 2)
         # send baudrate cmd (required) & sync
-        baudrate = s.BAUDRATE_FAST if s.mode != s.MODE_OCD else s.BAUDRATE_INIT
+        baudrate = self.BAUDRATE_FAST if self.mode != self.MODE_OCD else self.BAUDRATE_INIT
         rl78_br = {115200: 0, 250000: 1, 500000: 2, 1000000: 3}[baudrate]
         # 21 = 2.1v
         # really just sets internal voltage regulator to output 1.7, 1.8 or 2.1 volts
         # regulator seems to auto-adjust anyways...
         # feeding with 1.7v uses slower mode, 1.8v and 2.1v are same, slightly faster speed
-        r = s.a.set_baudrate(rl78_br, 21)
-        s.port.baudrate = baudrate
+        r = self.a.set_baudrate(rl78_br, 21)
+        self.port.baudrate = baudrate
         if r[0] != ProtoA.ST_ACK: return False
         delay(.01)
-        if s.mode != s.MODE_OCD:
-            r = s.a.reset()
+        if self.mode != self.MODE_OCD:
+            r = self.a.reset()
             if r[0] != ProtoA.ST_ACK: return False
         else:
-            s.ocd.wait_ack()
-            if not s.ocd.ping(): return False
+            self.ocd.wait_ack()
+            if not self.ocd.ping(): return False
         return True
 
 
