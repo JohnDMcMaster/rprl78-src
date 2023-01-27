88ffa6d0b8c48aaf23fa4270525e753515e003bd
John McMaster <johndmcmaster@gmail.com>
2023-01-27 15:47:50 -0600

    sync script
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- rp2040/env.sh
+++ rp2040/env.sh
@@ -2,3 +2,4 @@ PORT=/dev/ttyACM0
 # isn't this a virual UART
 BAUD=115200
 
+AMPY="ampy --port $PORT --baud $BAUD"
--- rp2040/sync.sh
+++ rp2040/sync.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
+# About 35 sec
+# Is there an "rsync" version thats quicker?
 
 source env.sh
+
+echo "Cleaning board..."
+# is there a way to quickly nuke the whole board?
+# $AMPY rmdir / 2>/dev/null || true
+echo "Removing rl78/..."
+$AMPY rmdir rl78 2>/dev/null || true
+echo "Removing files..."
+for f in $($AMPY ls); do
+    echo "  $f..."
+    $AMPY rm $f
+done
+
+echo "Begin upload"
+$AMPY mkdir rl78
 
 # Full sync is somewhat slow
 # Consider something like
 # ampy --port /dev/ttyACM0 --baud 115200 put probe.py
-for f in $(ls *.py */*.py); do
-    echo "Uploading $f..."
-    ampy --port $PORT --baud $BAUD put $f
+echo "Uploading scripts..."
+for f in $(ls *.py); do
+    echo "  $f..."
+    $AMPY put $f
 done
 
+echo "Uploading rl78/..."
+$AMPY put rl78/
+
+echo "Done!"
